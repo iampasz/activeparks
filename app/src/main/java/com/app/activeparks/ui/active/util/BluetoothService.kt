b11f8ec2a2d95e1c2db4247dafaae070c6e73893
pasz <saveseam@gmail.com>
2023-12-26 14:07:35 +0200

    Create file Bluetoothservice.kt

---
+++ app/src/main/java/com/app/activeparks/ui/active/util/BluetoothService.kt
@@ -0,0 +1,93 @@
+package com.app.activeparks.ui.active.util
+
+import android.annotation.SuppressLint
+import android.app.Service
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import com.app.activeparks.ui.active.model.ActivityState
+
+class BluetoothService : Service() {
+
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private lateinit var bluetoothManager: BluetoothManager
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private var device: BluetoothDevice? = null
+
+    private var activeState: ActivityState? = null
+
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.i("MAIN_ACTIVITY", "BLE SERVICE onCreate")
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("BLUETOOTH_SERVICE", "BluetoothService started")
+        return START_STICKY
+    }
+
+    inner class LocalBinder : Binder() {
+        fun getService(): BluetoothService = this@BluetoothService
+    }
+
+
+    @SuppressLint("MissingPermission")
+    fun connectToDevice(bluetoothGattCallback: BluetoothGattCallback) {
+        device?.connectGatt(this@BluetoothService, false, bluetoothGattCallback)
+    }
+
+    override fun onBind(intent: Intent?): IBinder {
+        return LocalBinder()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun scanBluetoothDevises(scanCallBack: ScanCallback) {
+        Log.i("BLUETOOTH_SERVICE", "Scan bluetooth devises")
+        bluetoothManager =
+            this@BluetoothService.getSystemService(BluetoothManager::class.java)
+        bluetoothAdapter = bluetoothManager.adapter
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        bluetoothLeScanner.startScan(scanCallBack)
+    }
+
+    @SuppressLint("MissingPermission")
+    fun stopScanBluetoothDevises(scanCallBack: ScanCallback) {
+        Log.i("BLUETOOTH_SERVICE", "Stop s can bluetooth devises")
+        bluetoothLeScanner.stopScan(scanCallBack)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("BLUETOOTH_SERVICE", "Service destroyed")
+
+        Log.i("MAIN_ACTIVITY", "BLE SERVICE Destroyed")
+    }
+
+
+    fun setDevise(device: BluetoothDevice) {
+        this.device = device
+    }
+
+//    fun getDevise(): BluetoothDevice? {
+//        return device
+//    }
+
+    fun setActiveState(activeState: ActivityState) {
+        this.activeState = activeState
+    }
+
+    fun getGetActiveState(): ActivityState? {
+        return activeState
+    }
+
+}
